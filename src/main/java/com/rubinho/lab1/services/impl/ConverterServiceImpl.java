4985fad859fa28f5088c3361914181b6f73d6630
rubinho-m <rubinho-m@yandex.ru>
2024-12-03 15:01:58 +0300

    fix json

--- src/main/java/com/rubinho/lab1/services/impl/ConverterServiceImpl.java
+++ src/main/java/com/rubinho/lab1/services/impl/ConverterServiceImpl.java
@@ -2,21 +2,17 @@ package com.rubinho.lab1.services.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.rubinho.lab1.dto.ProductDto;
 import com.rubinho.lab1.services.ConverterService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ConverterServiceImpl implements ConverterService {
-    private final ObjectMapper objectMapper;
-
-    @Autowired
-    public ConverterServiceImpl(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
+    private final ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
 
     @Override
     public List<ProductDto> toList(String content) throws JsonProcessingException {
